f1c4395e444acbbd6a6cecf60fbab57fa65ce96a
Saurav Agarwal <agr.saurav1@gmail.com>
2025-06-10 14:49:55 -0400

    add datestamps

--- build_all_images.sh
+++ build_all_images.sh
@@ -2,31 +2,32 @@
 
 set -x
 
+DATESTAMP=$(date -u +'%Y%m%dZ')
 # Get latest CoverageControl repo from dev branch
 wget https://github.com/KumarRobotics/CoverageControl/archive/refs/heads/dev.zip -O CoverageControl.zip
-FROM_IMAGE_TAG=noble-torch2.5.1-jazzy
-docker buildx build --no-cache -t agarwalsaurav/pac:jazzy -t agarwalsaurav/pac:latest --build-arg IMAGE_TAG=${FROM_IMAGE_TAG} --push .
+FROM_IMAGE_TAG=noble-torch2.5.1-jazzy-20250610Z
+docker buildx build --no-cache -t agarwalsaurav/pac:jazzy-${DATESTAMP} --build-arg IMAGE_TAG=${FROM_IMAGE_TAG} --push .
 if [ $? -ne 0 ]; then
     echo "Failed to build image"
     exit 1
 fi
 
-FROM_IMAGE_TAG=jammy-torch2.5.1-humble
-docker buildx build --no-cache -t agarwalsaurav/pac:humble --build-arg IMAGE_TAG=${FROM_IMAGE_TAG} --push .
+FROM_IMAGE_TAG=jammy-torch2.5.1-humble-20250610Z
+docker buildx build --no-cache -t agarwalsaurav/pac:humble-${DATESTAMP} --build-arg IMAGE_TAG=${FROM_IMAGE_TAG} --push .
 if [ $? -ne 0 ]; then
     echo "Failed to build image"
     exit 1
 fi
 
-FROM_IMAGE_TAG=arm64-jammy-torch2.5.1-humble
-docker buildx build --no-cache -t agarwalsaurav/pac:arm64-humble --platform linux/arm64 --build-arg IMAGE_TAG=${FROM_IMAGE_TAG} --push .
+FROM_IMAGE_TAG=arm64-jammy-torch2.5.1-humble-20250610Z
+docker buildx build --no-cache -t agarwalsaurav/pac:arm64-humble-${DATESTAMP} --platform linux/arm64 --build-arg IMAGE_TAG=${FROM_IMAGE_TAG} --push .
 if [ $? -ne 0 ]; then
     echo "Failed to build image"
     exit 1
 fi
 
-FROM_IMAGE_TAG=arm64-noble-torch2.5.1-jazzy
-docker buildx build --no-cache -t agarwalsaurav/pac:arm64-jazzy --platform linux/arm64 --build-arg IMAGE_TAG=${FROM_IMAGE_TAG} --push .
+FROM_IMAGE_TAG=arm64-noble-torch2.5.1-jazzy-20250610Z
+docker buildx build --no-cache -t agarwalsaurav/pac:arm64-jazzy-${DATESTAMP} --platform linux/arm64 --build-arg IMAGE_TAG=${FROM_IMAGE_TAG} --push .
 if [ $? -ne 0 ]; then
     echo "Failed to build image"
     exit 1
